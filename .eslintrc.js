136a208c806474321c670c20f868c340b8cc8b1b
nokazn <nokazn@gmail.com>
2020-08-25 02:45:12 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    'import/extensions': [
+      2,
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        json: 'never',
+      },
+    ],
+    indent: [2, 2],
+    'lines-between-class-members': 0,
+    'no-console': 0,
+    // typescript-eslint の no-unuserd-vars を有効にする
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+  },
+};
