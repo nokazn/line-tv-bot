a760869b59851a861a7bb3facd6f815b9ce92b86
nokazn <41154684+nokazn@users.noreply.github.com>
2022-01-23 02:55:21 +0900

    chore: fix ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const ALLOW = 0;
+const WARN = 1;
+const ERROR = 2;
+
 module.exports = {
   env: {
     es6: true,
@@ -24,15 +28,36 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'jest'],
   rules: {
-    'lines-between-class-members': 0,
-    'no-console': 1,
-    'arrow-body-style': 0,
+    'lines-between-class-members': ALLOW,
+    'no-console': WARN,
+    'arrow-body-style': ALLOW,
+
+    // airbnb のスタイルから for of 文の禁止の部分を削除
+    // See also https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L337-L357
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
 
     /**
      * eslint-plugin-import
      */
     'import/extensions': [
-      2,
+      ERROR,
       {
         ts: 'never',
         tsx: 'never',
@@ -41,16 +66,16 @@ module.exports = {
         json: 'never',
       },
     ],
-    'import/no-unresolved': 0,
+    'import/no-unresolved': ALLOW,
     // typescript-eslint の no-use-before-define を有効にする
-    'no-use-before-define': 0,
-    '@typescript-eslint/no-use-before-define': 2,
+    'no-use-before-define': ALLOW,
+    '@typescript-eslint/no-use-before-define': ERROR,
     // 'import/no-unresolved': [1, { commonjs: true, amd: true }],
     'import/no-extraneous-dependencies': [1, { devDependencies: true }],
-    'import/prefer-default-export': 0,
+    'import/prefer-default-export': ALLOW,
 
     // typescript-eslint の no-unuserd-vars を有効にする
-    'no-unused-vars': 0,
-    '@typescript-eslint/no-unused-vars': 2,
+    'no-unused-vars': ALLOW,
+    '@typescript-eslint/no-unused-vars': ERROR,
   },
 };
