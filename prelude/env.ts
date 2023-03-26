72b3a8fd03a8583578cafeb88ddf26ce152691cc
nokazn <41154684+nokazn@users.noreply.github.com>
2023-03-26 11:56:41 +0900

    chore: fix env resolution for dev

--- prelude/env.ts
+++ prelude/env.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
 import { validateEnv } from '../src/schemas/env';
 
 if (process.env.NODE_ENV !== 'production') {
-  import('dotenv/config');
+  dotenv.config();
 }
 
+import('dotenv/config');
 validateEnv(process.env);
