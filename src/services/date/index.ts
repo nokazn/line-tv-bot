99e584067f6bd4f722be01f9329cb32c32e5f874
nokazn <41154684+nokazn@users.noreply.github.com>
2023-04-27 00:16:40 +0900

    chore: enable japanese date format

--- src/services/date/index.ts
+++ src/services/date/index.ts
@@ -1,3 +1,8 @@
 import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import 'dayjs/locale/ja';
+
+dayjs.locale('ja');
+dayjs.extend(customParseFormat);
 
 export { dayjs };
