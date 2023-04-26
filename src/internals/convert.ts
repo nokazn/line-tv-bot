a66b6ed5330f26024d58a1871fb7805dceccadc9
nokazn <41154684+nokazn@users.noreply.github.com>
2023-04-27 00:17:06 +0900

    chore: add date formatter

--- src/internals/convert.ts
+++ src/internals/convert.ts
@@ -1,3 +1,6 @@
+import { err, ok } from 'neverthrow';
+import { dayjs } from '~/services';
+
 import { CHANNEL_TYPES } from '~/constants/domain';
 import type { ProgramKeyword } from '~/schemas';
 import { Nullish } from '~/types';
@@ -34,3 +37,29 @@ export const convertKeywordToSearchUrl =
       channelType: extractChannelType(keyword.channelType),
     });
   };
+
+/**
+ * @description 番組の詳細ページがら取得した日付をパースする
+ */
+export const parseProgramDate = (text: string) => {
+  const [rawDay, rawTime] = text.split(/\s+/);
+  if (!rawDay || !rawTime) {
+    return err(undefined);
+  }
+  const [rawFrom, rawTo] = rawTime.split('〜');
+  if (!rawFrom || !rawTo) {
+    return err(undefined);
+  }
+  // TODO: 年またいだ時の考慮
+  const day = dayjs(rawDay, 'M月D日(ddd)');
+  const from = dayjs(rawFrom, 'HH:mm').set('date', day.date()).set('month', day.month());
+  const to = dayjs(rawTo, 'HH:mm').set('date', day.date()).set('month', day.month());
+  if ([day, from, to].some((date) => !date.isValid())) {
+    return err(undefined);
+  }
+  return ok({
+    day: day.toDate(),
+    from: from.toDate(),
+    to: to.toDate(),
+  });
+};
